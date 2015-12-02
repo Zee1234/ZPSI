228dfde47656eb67a335e90f18781d52e432d7ab
Zee1234 <Zee1234@users.noreply.github.com>
2015-12-01 21:11:02 -0600

    Complete rewrite. Now supports nesting in arrays!

--- ZPSI.lua
+++ ZPSI.lua
@@ -21,112 +21,138 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 --]]
-ZPSI = {}
+local ZPSI = {}
+local StringFunctions = {}
 local OUTPUT = {}
 
-function StringFunctions(inputstring,method,e1,e2,e3,e4)
-  local switch = {
-    ["DashString"] = function(value) return string.match(value,"%-*") end,
-    ["FirstColon"] = function(value) return tonumber(string.match(string.find(value,"[^\\]:"),"%d*"))+1 end,
-    ["ValueText"] = function(value,position) return string.sub(value,position+2) end,
-    ["CheckIfNum"] = function(value) if string.match(value:match(".*%S"),"^%-?%d*%.?%d*$") then return true else return false end end,
-    ["CheckIfBool"] = function(value)
-      local a = string.match(value:lower(),"^true%s*$")
-      local c = string.match(value:lower(),"^false%s*$")
-      if a or c then
-        if a then
-          return true, true 
-        else
-          return true, false
-        end
-      else
-        return false 
-      end
-    end, 
-  }
-  return switch[method](inputstring,e1,e2,e3,e4)
-end
-
-function DepthArrayToFields(fieldarray,depth)
-  if (depth==0) then return "" end
-  return DepthArrayToFields(fieldarray,depth - 1) .. "." .. tostring(fieldarray[depth])
-end
-
-function SetField (fields,value,a_Array,arraynum)
-  local t = OUTPUT
-  for w, d in string.gmatch(fields, "([%w_]+)(.?)") do
-    if d == "." then
-      t[w] = t[w] or {}
-      t = t[w]
-    else
-      if a_Array then
-        t[arraynum] = value
-      else
-        t[w] = value
-      end
-    end
-  end
-end
-
-function Check_Type(input)
-  if(type(input) ~= "table") then return type(input) end
-  local i = 1
-  for _ in pairs(input) do
-    if input[i] ~= nil then i = i + 1 else return "table" end
-  end
-  if i == 1 then return "table" else return "array" end
-end
-
-function FindType(a_String,truncate)
-  local isNum, isBool, BoolVal = StringFunctions(a_String,"CheckIfNum"),StringFunctions(a_String,"CheckIfBool")
-  if isNum then return tonumber(string.match(a_String,".*%S")) end
-  if isBool then return BoolVal end
-  return if truncate then string.match(a_String,".*%S") else a_String end
-end
-
-function CheckValueOrObj(a_String,truncate)
-  if(not a_String:match("%g")) then
-    return {}
+function StringFunctions.RemoveComments(a_string)
+  local _,num1 = a_string:find("^%s*")
+  local _,num2 = a_string:find("#")
+  if num1 == (num2 or -1) - 1 then
+    return true, ""
   else
-    if truncate then
-      return FindType(a_String,true)
-    else
-      return FindType(a_String,false)
+    local mutil = a_string
+    local pos = 1
+    while mutil ~= "" do
+      local spot = mutil:find("#") or mutil:len()
+      pos, mutil = pos + spot, mutil:sub(spot-1,spot-1) == "\\" and mutil:sub(spot+1) or ""
     end
+    return false, a_string:sub(1,pos-1)
   end
 end
 
-function ZPSI.parse(filename,truncate)
-  if not truncate then truncate = true end
-  local DEPTHNAMES = {}
-  local file = io.open(filename)
-  local linenum = 1
-  local lastarrayline = 0
-  local arraynum = 1
-  for lines in file:lines() do
-    if string.match(lines,"^%S.*") and (string.sub(string.match(lines,"^%S.*"),1,1) ~= "#") then
-      local dashes = StringFunctions(lines,"DashString")
-      local depthnum = string.len(dashes)
-      local firstcolon = StringFunctions(lines,"FirstColon")
-      local value = StringFunctions(lines,"ValueText",firstcolon)
-      if(depthnum ~= (firstcolon - 1)) then
-        DEPTHNAMES[depthnum + 1] = string.sub(lines,depthnum+1,firstcolon-1)
-        SetField(DepthArrayToFields(DEPTHNAMES,depthnum+1),CheckValueOrObj(value,truncate),false)
-      else
-        if linenum ~= lastarrayline + 1 then arraynum = 1 end
-        DEPTHNAMES[depthnum + 1] = arraynum
-        SetField(DepthArrayToFields(DEPTHNAMES,depthnum+1),CheckValueOrObj(value,truncate),true,arraynum)
-        lastarrayline = linenum
-        arraynum = arraynum+1
-      end
-      linenum = linenum + 1
+function StringFunctions.FirstColon(a_string)
+  local x,y = string.match(string.find(a_string,"[^\\]:"),"%d*")
+  return y
+end
+
+function StringFunctions.GetType(a_string)
+  local bool, boolval = StringFunctions.CheckIfBool(a_string)
+  if bool then return boolval end
+  local num, numval = StringFunctions.CheckIfNum(a_string)
+  if num then return numval end
+  return a_string
+end
+
+function StringFunctions.CheckIfNum(a_string)
+  if string.match(a_string:match(".*%S"),"^%-?%d*%.?%d*$") then return true, tonumber(a_string) else return false end 
+end
+
+function StringFunctions.CheckIfBool(value)
+  local a = string.match(value:lower(),"^true%s*$")
+  local c = string.match(value:lower(),"^false%s*$")
+  if a or c then
+    if a then
+      return true, true 
+    else
+      return true, false
     end
+  else
+    return false 
+  end
+end
+
+function StringFunctions.GetDepth(a_string)
+  local _,num = string.find(a_string,"^%-*.")
+  return num - 1, string.sub(a_string,num)
+end
+
+function StringFunctions.GetKey(a_string)
+  local colon = a_string:find(":") or 0
+  if colon == 1 then return false, a_string:sub(2) end
+  return string.match(a_string:sub(1,colon-1),"[%w_]+"), a_string:sub(colon+1)
+end
+
+function StringFunctions.GetMess(a_string)
+  a_string = a_string:match( "^%s*(.-)%s*$" )
+  if a_string == "" then return {} end
+  if a_string:find("^\".*\"$") then
+    return a_string:sub(2,a_string:len()-1)
+  else
+    return StringFunctions.GetType(a_string)
+  end
+end
+
+function StringFunctions.Deconstruct(a_string)
+  local depth, key, message
+  depth, a_string = StringFunctions.GetDepth(a_string)
+  key, a_string = StringFunctions.GetKey(a_string)
+  message = StringFunctions.GetMess(a_string)
+  return depth,key,message
+end
+
+local SetValue = function(depth,store,value)
+  local t = OUTPUT
+  local w
+  for i = 0, depth - 1 do
+    w = store[i].name or store[i].lastarrelem + 1
+    t[w] = t[w] or {}
+    t = t[w]
+  end
+  w = store[depth].name or store[depth].lastarrelem + 1
+  t[w] = value
+end
+
+--[[
+{
+  name = {string}/nil
+  lastarrelem = {number}/nil
+  subarrpres = nil/true
+}
+--]]
+
+function ZPSI.parse(filename)
+  local store = {}
+  local file = io.open(filename)
+  local linenum = 0
+  local lastdepth = 0
+  local pure
+  for lines in file:lines() do
+    linenum = linenum + 1
+    pure,lines = StringFunctions.RemoveComments(lines)
+    repeat
+      if pure then break end
+      local depth,key,message = StringFunctions.Deconstruct(lines)
+      assert(depth <= lastdepth + 1, "Invalid key at line " .. tostring(linenum) .. "!")
+      lastdepth = depth
+      for i1 = #store, depth + 1, -1 do store[i1] = nil end
+      store[depth] = store[depth] or {}
+      if key then
+        store[depth].name = key
+      else
+        store[depth].name = nil
+        store[depth].lastarrelem = (store[depth].lastarrelem or -1) + 1
+        store[depth].subelempres = nil
+      end
+      local prev = store[depth-1] or {}
+      if prev.lastarrelem and not prev.subelempres then
+        store[depth-1].lastarrelem = store[depth-1].lastarrelem + 1 
+        store[depth-1].subelempres = true
+      end
+      SetValue(depth,store,message)
+    until true
   end
   return OUTPUT
-end
-
-function ZPSI.truncate(a_String)
-  return string.match(a_String,".*%S")
 end
 
 return ZPSI
